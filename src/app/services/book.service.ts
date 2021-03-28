e0b63a8efe6bd63ba05eff30b61b2ed27c60432f
Himu <mohsi.ahamed@gmail.com>
2021-03-28 15:09:36 +0600

    worked on unsubscribing observables.

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -14,29 +14,29 @@ import { book } from '../models/book';
 })
 export class BookService {
 
-books:book;
-baseurl = environment.apiUrl;
-constructor(private http:HttpClient) { }
+  books:book;
+  baseurl = environment.apiUrl;
+  constructor(private http:HttpClient) { }
 
-getAllBooks(){
-  return this.http.get<book[]>(this.baseurl+'getallbooks');
-}
+  getAllBooks():Observable<book[]>{
+    return this.http.get<book[]>(this.baseurl+'getallbooks');
+  }
 
-getBookById(id:number):Observable<book> {
-  return this.http.get<book>(this.baseurl+'getbook/'+id);
-}
+  getBookById(id:number):Observable<book> {
+    return this.http.get<book>(this.baseurl+'getbook/'+id);
+  }
 
-addBook(book:book)
-{
-  return this.http.post(this.baseurl+'addbook', book);
-}
+  addBook(book:book)
+  {
+    return this.http.post(this.baseurl+'addbook', book);
+  }
 
-editBook(book:book, id:number){
-  return this.http.put(this.baseurl+"editbook/"+id, book);
-}
+  editBook(book:book, id:number){
+    return this.http.put(this.baseurl+"editbook/"+id, book);
+  }
 
-deleteBook(id:number){
-  return this.http.delete(this.baseurl+'deletebook/'+id);
-}
+  deleteBook(id:number){
+    return this.http.delete(this.baseurl+'deletebook/'+id);
+  }
 
 }
