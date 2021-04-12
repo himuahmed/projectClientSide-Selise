e9a6dd05632456f6b5b11ce5c62658ad5b709438
Himu <mohsi.ahamed@gmail.com>
2021-04-12 13:23:16 +0600

    Implemented Caching through service worker.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,16 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatMenuModule} from '@angular/material/menu';
+import { CommonModule } from '@angular/common';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppComponent } from './app.component';
-
 import { BookModule } from './book/book.module';
-
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 import { appRoutes } from './routes';
-import { AuthenticationModule } from './authentication/authentication.module';
-import { CommonModule } from '@angular/common';
-import { ToolbarComponent } from './toolbar/toolbar.component';
+import { environment } from '../environments/environment';
 
 
 
@@ -30,7 +30,13 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
     MatToolbarModule,
     MatButtonModule,
     MatInputModule,
-    MatMenuModule
+    MatMenuModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
 
   ],
   bootstrap: [AppComponent]
